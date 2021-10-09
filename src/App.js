fdbc714a9ea017cbb3e493984ed05f9ed885c5d3
Kaung Phyo Wai <kaungphyowaidecember@gmail.com>
2021-10-09 15:08:02 +0630

    Coronavirus map with api of https://corona.lmao.ninja/v3/covid-19/countries complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { TileLayer, MapContainer, Marker, Popup } from "react-leaflet";
-import { geolocated } from "react-geolocated";
+import json from "./temp";
 import "./App.css";
 const DEFAULT_LATITUDE = 0;
-const DEFAULT_LANGITUDE = 0;
+const DEFAULT_LONGITUDE = 0;
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+  async componentDidMount() {
+    const url = "https://corona.lmao.ninja/v3/covid-19/countries";
+    const finalData = await fetch(url).then((response) => response.json());
+    this.setState({ data: finalData });
+  }
   render() {
-    
-    return !this.props.isGeolocationAvailable ? (
-      <div className="center">Your browser does not support Geolocation</div>
-    ) : !this.props.isGeolocationEnabled ? (
-      <div className="center">Geolocation is not enabled</div>
-    ) : this.props.coords ? (
+    return (
       <MapContainer
-        center={[this.props.coords.latitude, this.props.coords.longitude]}
+        center={[DEFAULT_LATITUDE, DEFAULT_LONGITUDE]}
         zoom={2}
         scrollWheelZoom={true}
       >
@@ -21,21 +27,37 @@ class App extends React.Component {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker
-          position={[this.props.coords.latitude, this.props.coords.longitude]}
-        >
-          <Popup>I am here.</Popup>
-        </Marker>
+        {this.state.data.length > 0 &&
+          this.state.data.map(function (data) {
+            return (
+              <Marker
+                position={[data.countryInfo.lat, data.countryInfo.long]}
+                key={data.country}
+              >
+                <Popup>
+                  <img
+                    src={data.countryInfo.flag}
+                    alt={data.country + " flag"}
+                    width="200"
+                    height="100"
+                  />
+                  <h2>{data.country}</h2>
+                  <h3 style={{ color: "red" }}>
+                    TodayCases😷️: {data.todayCases}
+                  </h3>
+                  <h4 style={{ color: "red" }}>active🤒️: {data.active}</h4>
+                  <h4 style={{ color: "red" }}>critical⚠️: {data.critical}</h4>
+                  <h4 style={{ color: "green" }}>
+                    recovered😍️: {data.recovered}
+                  </h4>
+                  <h4>deaths😥️: {data.deaths}</h4>
+                </Popup>
+              </Marker>
+            );
+          })}
       </MapContainer>
-    ) : (
-      <div className="center"> Loading.. </div>
     );
   }
 }
 
-export default geolocated({
-  positionOptions: {
-    enableHighAccuracy: false,
-  },
-  userDecisionTimeout: 5000,
-})(App);
+export default App;
